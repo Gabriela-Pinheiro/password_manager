8844a017f135ebb0514becdf5624bfd72264bec2
Gabriela-Pinheiro <pi.gabi@hotmail.com>
2023-09-01 14:30:19 +0100

    Changing TXT to JSON
    Manipulating a JSON file instead of TXT, to add the "search" function.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -22,39 +23,63 @@ def generate_password():
     return password_generated
 
 
-def password_populator():
+def populate_password():
     password_entry.delete(0, END)
     password = generate_password()
     password_entry.insert(0, password)
 
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search():
+    searched = (website_entry.get()).capitalize()
+    try:
+        with open("users_manager.json") as file:  # FileNotFound handler
+            data = json.load(file)
 
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File found")
 
-def empty_field_checker(website, email, password):
-    if len(website) == 0 or len(email) ==0 or len(password) ==0:
-        messagebox.showinfo(title="Ooops", message="Please, don't leave any fields empty!")
-        return False
     else:
-        return True
+        if searched in data:
+            messagebox.showinfo(title=searched, message=f"Email: {data[searched]['email']}\nPassword: {data[searched]['password']}")
+        else:
+                    messagebox.showinfo(title="Error", message=f"No {searched.upper()} credential found!\nTry and ADD it instead.")
 
 
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     """Add the info in the form onto a text file"""
-    website = website_entry.get()
-    email = email_entry.get()
+    website = (website_entry.get()).capitalize()
+    email = (email_entry.get()).lower()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+    }}
 
-    if empty_field_checker(website, email, password):
-        # messagebox.showinfo(title="Message", message="Message2")
-        ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}\nPassword: {password}\n\nIs it ok to save?")
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Ooops", message="Please, don't leave any fields empty!")
+    else:
+        try:
+            with open("users_manager.json", mode="r") as file:
+                data = json.load(file)  # read mode "r"
+                data.update(new_data)  # update the file
 
-        if ok:
-            with open("gabs_manager.txt", mode="a") as file:
-                file.write(f"{website:15} | {email:30} | {password}\n")
+        except FileNotFoundError:
+            with open("users_manager.json", mode="w") as file:
+                json.dump(new_data, file, indent=4)  # write mode="w"
 
+        else:
+            data.update(new_data)
+
+            with open("users_manager.json", mode="w") as file:
+                json.dump(data, file, indent=4)  # write mode="w"
+
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -79,19 +104,22 @@ password_label.grid(column=0, row=3)
 
 
 ### Entries
-website_entry = Entry(width=51)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=32)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_entry = Entry(width=51)
 email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "gabriela.pinheiro@outlook.com")  # index END end of the sentence
+email_entry.insert(0, "name.surname@outlook.com")  # index END end of the sentence
 
 password_entry = Entry(width=32)
 password_entry.grid(column=1, row=3)
 
 ### Buttons
-generate_button = Button(text="Generate Password", command=password_populator)
+search_button = Button(text="Search", width=14, command=search)
+search_button.grid(column=2, row=1)
+
+generate_button = Button(text="Generate Password", command=populate_password)
 generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=44, command=save)
